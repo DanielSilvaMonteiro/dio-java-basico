c97ea2d3aced6b9ac5c8d8d9eda63cc7a1ab4669
monte@danielmonteiro <monteiro.dan@outlook.com>
2024-08-02 21:53:19 -0300

    operadores novos adicionados

--- java-introducao/src/Operadores.java
+++ java-introducao/src/Operadores.java
@@ -16,6 +16,28 @@ public class Operadores {
         System.out.println(concatenacao);// 1111
         concatenacao = "1" + (1 + 1 + 1);
         System.out.println(concatenacao);// 13
+
+        /************************************************************* */
+        int numero = 5;
+
+        // Imprimindo o numero negativo
+        System.out.println(-numero);
+
+        // incrementando numero em mais 1 numero, imprime 6
+        numero++;
+        System.out.println(numero);
+
+        // incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        // ordem de precedencia conta aqui
+        System.out.println(++numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
     }
 
 }
