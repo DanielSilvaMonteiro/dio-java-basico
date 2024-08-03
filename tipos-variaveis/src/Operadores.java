26a7802ff2f427b728adebf24cf7003f01448fe4
monte@danielmonteiro <monteiro.dan@outlook.com>
2024-08-02 21:39:18 -0300

    commit operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,6 +1,21 @@
 public class Operadores {
     public static void main(String[] args) {
 
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(concatenacao);// 31
+        concatenacao = 1 + "1" + 1 + 1;
+        System.out.println(concatenacao);// 1111
+        concatenacao = 1 + "1" + 1 + "1";
+        System.out.println(concatenacao);// 1111
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);// 1111
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);// 13
     }
 
 }
