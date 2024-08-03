67280c52fb5a6cd822d65fbf42be799ff66e1caf
monte@danielmonteiro <monteiro.dan@outlook.com>
2024-08-03 01:14:16 -0300

    exemplo de doc executado no terminal

---
+++ doc-java-calculadora/src/ExemploDocJava.java
@@ -0,0 +1,7 @@
+public class ExemploDocJava {
+    public static void main(String[] args) {
+        System.out.println("rodar no terminal");
+        System.out.println("javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java");
+    }
+
+}
