8436c7b4a54f7cb2f077f92db68781d1b203cc86
monte@danielmonteiro <monteiro.dan@outlook.com>
2024-08-06 23:49:24 -0300

    else adicionado em uma estrutura simples, ficando uma estrutura de decisao composta

--- estrutura-de-repeticao/repeticao-simples/src/ContaBancaria.java
+++ estrutura-de-repeticao/repeticao-simples/src/ContaBancaria.java
@@ -1,10 +1,12 @@
 public class ContaBancaria {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 27.0;
 
         if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+        } else {
+            System.out.println("Saldo Insuficiente");
         }
 
         System.out.println("Saldo atual: " + saldo);
